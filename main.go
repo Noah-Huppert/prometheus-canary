c9bb41b4e00c5a3ff80b84f3e2c70fc616b8eef4
Noah Huppert <contact@noahh.io>
2020-09-29 01:57:06 -0400

    wip config

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/Noah-Huppert/goconf"
 )
 
+// Config determines the details of prometheus canary metrics.
+type Config struct {
+	// Metrics
+	Metrics []struct {
+		// BaseTime is the time of day when an alert will fire.
+		BaseTime time.Time `validate:"required" default:"T00:00:00Z"`
+	} `validate:"required"`
+}
+
 func main() {
-	fmt.Println("hello world")
+	// Load configuration
+	loader := goconf.NewLoader()
+
+	loader.AddConfigPath("/etc/foo/foo.*")
+	loader.AddConfigPath("/etc/foo.d/*")
+
+	config := Config{}
+	err := loader.Load(&config)
+	panic(err)
 }
